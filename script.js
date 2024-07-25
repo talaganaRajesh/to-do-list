6f4f4b31cbf9f9fceaaff65a7784da0cfe14aabb
Rajesh <141362321+talaganaRajesh@users.noreply.github.com>
2024-07-25 22:14:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputBox=document.getElementById('input-box');
+const listContainer=document.getElementById('list-container');
+
+function addTask(){
+    const task=inputBox.value;
+    if(task){
+        const li=document.createElement('li');
+        li.innerHTML=task;
+        listContainer.appendChild(li);
+        inputBox.value='';
+        let span=document.createElement('span');
+        span.innerHTML='\u00D7';
+        li.appendChild(span);
+    }
+    else{
+        alert('write something to continue !');
+    }
+
+    saveData();
+}
+
+listContainer.addEventListener('click',function(e){
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    if(e.target.tagName==='SPAN'){
+            e.target.parentElement.remove();
+            saveData();
+        }
+});
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showData(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+
+showData();
